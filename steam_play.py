735edb80da0f04aa40111e284d60108329aa35f9
Thorsten Sick <thorsten.sick@email.de>
2014-02-27 22:41:56 +0100

    base file added, includes todo

---
+++ steam_play.py
@@ -0,0 +1,19 @@
+#!python
+
+# Steampunk style punch card music player
+#
+# DONE
+#
+# TODO Read MP3 files and create music DB
+# TODO Create Playlist based on albums
+# TODO Print new cards from playlist (mark printed ones as already printed)
+# TODO Request selection of playlist to play (by id)
+# TODO play music
+# TODO User input by external knobs
+# TODO Output to external dials
+
+import argparse
+
+
+if __name_ == "__main__":
+    pass
