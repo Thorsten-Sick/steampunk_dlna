2de490b1ff7e325c242ce243590ea531c7d7852c
Thorsten Sick <thorsten.sick@email.de>
2014-03-02 16:46:34 +0100

    Removed a todo

--- steam_play.py
+++ steam_play.py
@@ -5,7 +5,6 @@
 # DONE
 #
 
-# TODO Print new cards from playlist (mark printed ones as already printed)
 # TODO Request selection of playlist to play (by id)
 # TODO User input by external knobs
 # TODO Output to external dials
